9431337db20958243f4d3e6d0fb35c6d3ed8ce45
Joshua R Day <day_joshua_r@lilly.com>
2024-11-19 15:38:31 -0500

    try this one

--- single_file_check.py
+++ single_file_check.py
@@ -2,9 +2,9 @@ import requests as req
 
 def main():
     print("A single file with a vulnerability.")
-    xyz = input("Enter a value: ")
+    xyz = input("Enter a domain: ")
     print(f"You entered {xyz}")
-    q = f"https://www.google.com/search?q={xyz}"
+    q = f"https://{xyz}"
     print(f"The URL is: {q}")
     x = req.get(q)
     print(f"{x.status_code}")
